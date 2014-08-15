05025a935faca371e55944e410d81765a9c1cf1a
janstenpickle <jansen.chris@gmail.com>
2014-08-15 21:56:44 +0100

    Add project functions to cli

--- lib/shanty/cli.rb
+++ lib/shanty/cli.rb
@@ -1,6 +1,18 @@
 require 'thor'
+require 'shanty'
+require 'shanty/util'
 
 module Shanty
+  # Cli
   class CLI < Thor
+    desc 'projects', 'Lists projects discovered by shanty'
+    def projects
+      Util.logger.info(Shanty.new.graph.projects.inspect)
+    end
+
+    desc 'changed_projects', 'Lists changed projects discovered by shanty'
+    def changed_projects
+      Util.logger.info(Shanty.new.graph.changed.inspect)
+    end
   end
 end
