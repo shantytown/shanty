d7f944441a94dac5a796a9a4fb9628c66efa8808
Nathan Kleyn <nathan@nathankleyn.com>
2015-09-28 09:21:32 +0100

    (#18) Reorganise the specs for Artifact.

--- spec/lib/shanty/artifact_spec.rb
+++ spec/lib/shanty/artifact_spec.rb
@@ -3,37 +3,41 @@ require 'shanty/artifact'
 # All classes referenced belong to the shanty project
 module Shanty
   RSpec.describe(Artifact) do
-    subject { Artifact.new('html', 'test', uri) }
+    subject { described_class.new('html', 'test', uri) }
+    let(:relative_path_artifact) { described_class.new('html', 'test', URI('file:kim/cage.html')) }
+    let(:http_artifact) { described_class.new('html', 'test', URI('http://www.nic.com/kim/cage.html')) }
+    let(:schemless_artifact) { described_class.new('html', 'test', URI('kim/cage.html')) }
 
-    describe('#localfile') do
-      let(:uri) { URI('file:///nic/kim/cage.html') }
+    let(:uri) { URI('file:///nic/kim/cage.html') }
 
-      it('works with a local file') do
-        expect(subject.local?).to be(true)
-        expect(subject.to_local_path).to eql('/nic/kim/cage.html')
-      end
-    end
-
-    describe('#noscheme') do
-      let(:uri) { URI('kim/cage.html') }
-      it('fails if there is no scheme present in the URI') do
-        expect { subject }.to raise_error('Scheme not present on URI')
-      end
-    end
-
-    describe('#notabsolute') do
-      let(:uri) { URI('file:kim/cage.html') }
+    describe('.new') do
       it('fails if the URI is not an absolute path') do
-        expect { subject }.to raise_error('URI is not absolute')
+        expect { relative_path_artifact }.to raise_error('URI is not absolute')
+      end
+
+      it('fails if there is no scheme present in the URI') do
+        expect { schemless_artifact }.to raise_error('Scheme not present on URI')
       end
     end
 
-    describe('#remotefile') do
-      let(:uri) { URI('http://www.nic.com/kim/cage.html') }
+    describe('#local?') do
+      it('returns false if the file is not a local file') do
+        expect(http_artifact.local?).to be(false)
+      end
 
-      it('fails when converting to a local path if the resource is not local') do
-        expect(subject.local?).to be(false)
+      it('returns true if the file is a local file') do
+        expect(subject.local?).to be(true)
+      end
+    end
+
+    describe('#to_local_path') do
+      it('throws an exception if the resource is not local') do
+        allow(subject).to receive(:local?).and_return(false)
         expect { subject.to_local_path }.to raise_error('URI is not a local resource')
+      end
+
+      it('returns the path if the resource is local') do
+        expect(subject.to_local_path).to eql('/nic/kim/cage.html')
       end
     end
   end
