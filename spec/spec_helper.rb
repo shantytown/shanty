1a27f9f371f2f7a7b486ae7d41aed5c47efd1e0e
Nathan Kleyn <nathan@nathankleyn.com>
2015-10-06 10:03:09 +0100

    Remove unused, unnamed RSpec context.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,32 +28,3 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 end
-
-RSpec.shared_context('') do
-  around do |example|
-    FileUtils.touch(File.join(root, '.shanty.yml'))
-    project_paths.each do |project_path|
-      FileUtils.mkdir_p(project_path)
-    end
-
-    Dir.chdir(root) do
-      example.run
-    end
-
-    FileUtils.rm_rf(root)
-  end
-
-  # We have to use `realpath` for this as, at least on Mac OS X, the temporary
-  # dir path that is returned is actually a symlink. Shanty resolves this
-  # internally, so if we want to compare to any of the paths correctly we'll
-  # need to resolve it too.
-  let(:root) { Pathname.new(Dir.mktmpdir('shanty-test')).realpath }
-  let(:project_paths) do
-    {
-      one: File.join(root, 'one'),
-      two: File.join(root, 'two'),
-      three: File.join(root, 'two', 'three')
-    }
-  end
-  let(:project_path) { project_paths.first }
-end
